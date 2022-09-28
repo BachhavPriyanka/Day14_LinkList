7bd9296383cf8b7a1ba4a3355cb91672018a4183
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-28 15:10:06 +0530

    Created a link list to add in between

--- Lists.java
+++ Lists.java
@@ -1,13 +1,16 @@
 package day14_LinkList;
 
 public class Lists {
-	 public static void main(String[] args) {
+	public class List {
+		public static void main(String[] args) {
 
 		    LinkedLists<Integer> linkedL = new LinkedLists<>();
 
-		    linkedL.addFirst(70);
-		    linkedL.addFirst(30);
-		    linkedL.addFirst(56);
+		    linkedL.addLast(56);
+		    linkedL.addLast(70);
 		    linkedL.display();
+		    linkedL.display();
+
 		  }
+	}
 }
