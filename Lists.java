15db555e957f090cddaa44f2a57662281ed76185
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-28 14:52:04 +0530

    Created a link list

---
+++ Lists.java
@@ -0,0 +1,12 @@
+package day14_LinkList;
+
+public class Lists {
+	public static void main(String[] args) {
+
+	    LinkedLists<Integer> linkedL = new LinkedLists<>();
+
+	    linkedL.addLast(56);
+	    linkedL.addLast(30);
+	    linkedL.addLast(70);
+	  }
+}
