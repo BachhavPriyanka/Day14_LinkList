b237f2c3ef09bf4977d8b2133565ef045cc96646
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-28 15:42:01 +0530

    LinkList

--- Lists.java
+++ Lists.java
@@ -1,16 +1,17 @@
 package day14_LinkList;
 
 public class Lists {
-	public class List {
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		    LinkedLists<Integer> linkedL = new LinkedLists<>();
+	    LinkedLists<Integer> linkedL = new LinkedLists<>();
 
-		    linkedL.addLast(56);
-		    linkedL.addLast(70);
-		    linkedL.display();
-		    linkedL.display();
+	    linkedL.addLast(56);
+	    linkedL.addLast(70);
+	    linkedL.display();
+	    linkedL.insertAt(2, 30);
+	    linkedL.display();
+	    linkedL.deleteFirst();
+	    linkedL.display();
 
-		  }
-	}
+	  }
 }
