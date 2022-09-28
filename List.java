0fad0b5af133d9098d294e21792ede1b0e44758a
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-28 15:05:56 +0530

    Created a link list

--- List.java
+++ List.java
@@ -1,21 +1,13 @@
 package day14_LinkList;
 
 public class List {
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 
-	    LinkedLists<Integer> linkedL = new LinkedLists<>();
+		    LinkedLists<Integer> linkedL = new LinkedLists<>();
 
-	    linkedL.addLast(56);
-	    linkedL.addLast(70);
-	    linkedL.display();
-	    linkedL.insertAt(2, 30);
-	    linkedL.display();
-	    linkedL.addNodeAfter(30, 40);
-	    linkedL.display();
-	    System.out.println("size is " + linkedL.size());
-	    linkedL.deleteNode(70);
-	    System.out.println("size is " + linkedL.size());
-	    linkedL.display();
-
-	  }
+		    linkedL.addFirst(70);
+		    linkedL.addFirst(30);
+		    linkedL.addFirst(56);
+		    linkedL.display();
+		  }
 }
